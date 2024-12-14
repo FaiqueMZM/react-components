5a3d5095e310351b2993374d809f4fb10063340c
Faique Inaas MZM <faiqueinaas@gmail.com>
2024-12-13 23:59:24 -0800

    added mobile view toggle for DefaultNav

--- src/components/NabBar/DefaultNav.tsx
+++ src/components/NabBar/DefaultNav.tsx
@@ -4,9 +4,14 @@ import logo from "../../assets/react.svg";
 
 const DefaultNav: React.FC = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Added state for mobile menu toggle
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const toggleMobileMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Toggle mobile menu visibility
   };
 
   return (
@@ -25,12 +30,17 @@ const DefaultNav: React.FC = () => {
           <button
             type="button"
             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-            aria-expanded="false"
+            aria-expanded={isMenuOpen ? "true" : "false"}
+            onClick={toggleMobileMenu} // Toggle mobile menu visibility
           >
             <span className="sr-only">Open main menu</span>
             <Bars3Icon className="w-5 h-5" aria-hidden="true" />
           </button>
-          <div className="hidden w-full md:block md:w-auto">
+          <div
+            className={`${
+              isMenuOpen ? "block" : "hidden"
+            } md:block w-full md:w-auto`} // Conditionally render the menu based on the state
+          >
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
               <li>
                 <a
