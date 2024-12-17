6ec9b9c39d7ce261f04141e1b0abaeb8ed2f57f5
Faique Inaas MZM <faiqueinaas@gmail.com>
2024-12-17 15:34:39 -0800

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavbarPage from "./pages/NavBarPage";
 import BotNavbarPage from "./pages/BotNavBarPage";
 import HomePage from "./pages/HomePage";
