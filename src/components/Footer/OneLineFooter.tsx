80cf8d62bd00d0c30916032b4def18f5eeba6969
Faique Inaas MZM <faiqueinaas@gmail.com>
2024-12-25 16:06:09 +0530

    updated one line footer component

--- src/components/Footer/OneLineFooter.tsx
+++ src/components/Footer/OneLineFooter.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import logo from "../../assets/react.svg";
 
 const OneLineFooter: React.FC = () => {
   return (
     <>
-      <div>OneLineFooter</div>
+      <footer className="bg-white">
+        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <div className="flex justify-center text-teal-600 sm:justify-start">
+              <img src={logo} className="h-8" alt="react logo" />
+              <span className="ml-2 text-xl font-bold tracking-wide text-gray-800">
+                OneLineFooter
+              </span>
+            </div>
+            <p className="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
+              Copyright &copy; 2022. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
