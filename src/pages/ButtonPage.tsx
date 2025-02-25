0021a8243af222d529faefb71865332158abe662
Faique Inaas MZM <faiqueinaas@gmail.com>
2025-02-25 21:55:34 +0530

    buttonpage

--- src/pages/ButtonPage.tsx
+++ src/pages/ButtonPage.tsx
@@ -23,14 +23,38 @@ const ButtonPage: React.FC = () => {
                 Danger Button
             </Button>
         ),
+        success: (
+            <Button
+                color="bg-green-500 text-white"
+                onClick={() => alert("Success Clicked!")}
+            >
+                Success Button
+            </Button>
+        ),
+        warning: (
+            <Button
+                color="bg-yellow-500 text-black"
+                onClick={() => alert("Warning Clicked!")}
+            >
+                Warning Button
+            </Button>
+        ),
     };
 
     return (
-        buttonVariations[variation || "default"] || (
-            <div className="text-red-500 text-center mt-4">
-                Button variation not found!
-            </div>
-        )
+        <div className="flex flex-col items-center space-y-4 mt-8">
+            <h1 className="text-2xl font-bold">Button Variations</h1>
+
+            {/* If a variation is provided, show only that button */}
+            {variation
+                ? buttonVariations[variation] || (
+                      <div className="text-red-500 text-center mt-4">
+                          Button variation not found!
+                      </div>
+                  )
+                : // Otherwise, show all buttons
+                  Object.values(buttonVariations)}
+        </div>
     );
 };
 
